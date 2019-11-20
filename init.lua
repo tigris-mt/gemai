a97bb2ccce0a9bd51c520750eca8ac00dc9eecca
Beha <shacknetisp@mail.com>
2019-11-20 18:33:52 -0500

    No aurum_mobs

--- init.lua
+++ init.lua
@@ -1,12 +1,11 @@
 gemai = {}
 
 -- Convert object reference to serializable table.
+-- Extend in mods implementing mobs/etc.
 -- Returns nil if unable to convert.
 function gemai.ref_to_table(obj)
 	if obj:is_player() then
 		return {type = "player", id = obj:get_player_name()}
-	elseif obj:get_luaentity() and obj:get_luaentity()._aurum_mobs_id then
-		return {type = "aurum_mob", id = obj:get_luaentity()._aurum_mobs_id}
 	end
 end
 
