f78dc816ffd6f56cd1c9aecec7425ac07b249daa
Beha <shacknetisp@mail.com>
2020-03-30 09:27:49 -0400

    Add always action that always fires an event.

--- actions.lua
+++ actions.lua
@@ -3,3 +3,7 @@ gemai.actions = {}
 function gemai.register_action(name, value)
 	gemai.actions[name] = value
 end
+
+gemai.register_action("gemai:always", function(self)
+	self:fire_event("gemai:always")
+end)
