edae0273525d0962eeb3e575f54b3fc152bb5b38
Beha <shacknetisp@mail.com>
2020-04-14 10:42:47 -0400

    Remove gemai.ref_to_table

--- init.lua
+++ init.lua
@@ -1,14 +1,5 @@
 gemai = {}
 
--- Convert object reference to serializable table.
--- Extend in mods implementing mobs/etc.
--- Returns nil if unable to convert.
-function gemai.ref_to_table(obj)
-	if obj:is_player() then
-		return {type = "player", id = obj:get_player_name()}
-	end
-end
-
 b.dofile("actions.lua")
 b.dofile("context.lua")
 
